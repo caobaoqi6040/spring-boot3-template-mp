b2569002f3dc707610bf5d14a28920a5ab0c984b
caobaoqi6040 <caobaoqi6040@gmail.com>
2025-09-26 17:04:10 +0800

    :tada: feat(core): 好的开始

---
+++ src/main/java/dev/caobaoqi6040/backend/SpringBoot3TemplateMpApplication.java
@@ -0,0 +1,13 @@
+package dev.caobaoqi6040.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3TemplateMpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3TemplateMpApplication.class, args);
+    }
+
+}
