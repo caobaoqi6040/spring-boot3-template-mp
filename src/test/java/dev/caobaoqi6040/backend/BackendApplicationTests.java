60d8d2d2c1c186ead26280c861011eaa78db29f7
caobaoqi6040 <caobaoqi6040@gmail.com>
2025-09-26 17:30:26 +0800

    :sparkles: feat(core): init spring security

--- src/test/java/dev/caobaoqi6040/backend/BackendApplicationTests.java
+++ src/test/java/dev/caobaoqi6040/backend/BackendApplicationTests.java
@@ -1,13 +1,25 @@
 package dev.caobaoqi6040.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class BackendApplicationTests {
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	@Test
 	void contextLoad() {
 
 	}
+
+	@Test
+	void generate_test_pwd() {
+		System.out.println(passwordEncoder.encode("pwd-admin"));
+		System.out.println(passwordEncoder.encode("pwd-general"));
+	}
+
 }
